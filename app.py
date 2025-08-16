6ad8eb6a94b4b54534b32caee1053bdbd9bbf51b
sushama shinde <sushamashinde0009@gmail.com>
2025-08-16 15:45:04 +0530

    webapp made

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pickle
+# scaler = pickle.load('scaler.pkl')
 
 from flask import Flask, request, jsonify, url_for, render_template
 import numpy as np
@@ -6,6 +7,7 @@ import pandas as pd
 
 app = Flask(__name__)
 regmodel = pickle.load(open('regmodel.pkl','rb'))
+scaler=pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -16,11 +18,43 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonyfy(output[0])
 
+# @app.route('/predict',methods=['POST'])
+# def predict():
+#     data=[float(x) for x in request.form.values()]
+#     final_input = scaler.transform(np.array(data).reshape(1,-1))
+#     print(final_input)
+#     output=regmodel.predict(final_input)[0]
+#     return render_template("home.html",prediction_test="The House Price Prediction Is {}".format(output))
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get form data
+    features = [
+        float(request.form['MedInc']),
+        float(request.form['HouseAge']),
+        float(request.form['AveRooms']),
+        float(request.form['AveBedrms']),
+        float(request.form['Population']),
+        float(request.form['AveOccup']),
+        float(request.form['Latitude']),
+        float(request.form['Longitude'])
+    ]
+    
+    # Transform features
+    final_features = scaler.transform(np.array(features).reshape(1, -1))  # Fixed variable name
+    
+    # Make prediction
+    prediction = regmodel.predict(final_features)
+    
+    return render_template('home.html', prediction=prediction)
+
+ 
 if __name__=="__main__":
     app.run(debug=True)
 
